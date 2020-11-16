28f7c60a326eece324e1c5647d679de5c137d2b7
Christer Vinje Gimse <68919499+drop0x55@users.noreply.github.com>
2020-11-16 15:10:15 +0100

    update fotoweb.js and index.html

--- dist/fotoweb.js
+++ dist/fotoweb.js
@@ -1,4 +1,4 @@
-var settings = {
+var fw1 = {
   "url": "https://foto.munchmuseet.no/fotoweb/archives/5000-Malerier/Assets/Arkiv/M0112_20190403.tif.info",
   "method": "GET",
   "timeout": 0,
@@ -6,10 +6,22 @@ var settings = {
     "Accept": "application/vnd.fotoware.asset+json"
   },
 };
+var fw1 = {
+  "url": "https://foto.munchmuseet.no/fotoweb/archives/5000-Malerier/Assets/Arkiv/M0651_20160414.tif.info",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Accept": "application/vnd.fotoware.asset+json"
+  },
+};
 
-$.ajax(settings).done(function (response) {
+$.ajax(fw1).done(function (response) {
   $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
   $("#FotoWeb1").attr("src",$jpg);
   console.log($jpg);
 });
-
+$.ajax(fw2).done(function (response) {
+  $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
+  $("#FotoWeb2").attr("src",$jpg);
+  console.log($jpg);
+});
