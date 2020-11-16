38429af2c464499177faa59d45b4462af7540ce7
Christer Vinje Gimse <68919499+drop0x55@users.noreply.github.com>
2020-11-16 13:41:23 +0100

    added fotoweb.js + update html

---
+++ dist/fotoweb.js
@@ -0,0 +1,15 @@
+var settings = {
+  "url": "https://foto.munchmuseet.no/fotoweb/archives/5000-Malerier/Assets/Arkiv/M0112_20190403.tif.info",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Accept": "application/vnd.fotoware.asset+json"
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
+  $("#FotoWeb1").attr("src",$jpg);
+  console.log($jpg);
+});
+
