6acb3e7c20afa2bff7afc9ea5f8602ae4d8f9589
Christer Vinje Gimse <68919499+drop0x55@users.noreply.github.com>
2021-04-28 14:06:28 +0200

    Update fotoweb.js
    Added new images from the FotoWeb API.

--- dist/fotoweb.js
+++ dist/fotoweb.js
@@ -22,7 +22,31 @@ var fw3 = {
     "Accept": "application/vnd.fotoware.asset+json"
   },
 };
+var fw4 = {
+  "url": "https://foto.munchmuseet.no/fotoweb/archives/5015-Munchs-tekster/Arkiv/No-MM%20T2789-01r.jpg.info",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Accept": "application/vnd.fotoware.asset+json"
+  },
+};
+var fw5 = {
+  "url": "https://foto.munchmuseet.no/fotoweb/archives/5015-Munchs-tekster/Arkiv/No-MM%20T2811-envelope.jpg.info",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Accept": "application/vnd.fotoware.asset+json"
+  },
+};
 
+var fw6 = {
+  "url": "https://foto.munchmuseet.no/fotoweb/archives/5015-Munchs-tekster/Arkiv/No-MM%20T2786-99-V.jpg.info",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Accept": "application/vnd.fotoware.asset+json"
+  },
+};
 $.ajax(fw1).done(function (response) {
   $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
   $("#FotoWeb1").attr("src",$jpg);
@@ -36,5 +60,20 @@ $.ajax(fw2).done(function (response) {
 $.ajax(fw3).done(function (response) {
   $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
   $("#FotoWeb3").attr("src",$jpg);
+  console.log($jpg);
+});
+$.ajax(fw4).done(function (response) {
+  $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
+  $("#FotoWeb4").attr("src",$jpg);
+  console.log($jpg);
+});
+$.ajax(fw5).done(function (response) {
+  $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
+  $("#FotoWeb5").attr("src",$jpg);
+  console.log($jpg);
+});
+$.ajax(fw6).done(function (response) {
+  $jpg = "https://foto.munchmuseet.no" + response.previews[7].href;
+  $("#FotoWeb6").attr("src",$jpg);
   console.log($jpg);
 });
